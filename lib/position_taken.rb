f3d5d9e59267458e06e91e9b06659fd6105ece9c
Tom Storm <tstorm@invoca.com>
2018-01-08 20:32:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,7 +5,7 @@ def position_taken?(board, index)
     return false
   elsif board[index] == ""
     return false
-  elsif board[index] ==
+  elsif board[index] == nil
     return false
   elsif board[index] == "X" || board[index] == "O"
     return true
